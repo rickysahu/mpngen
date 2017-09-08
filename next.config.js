b977ae2efe3b4ff77aec4ab2b8155e94180dd553
EC2 Default User <ec2-user@ip-172-31-32-229.us-west-2.compute.internal>
2017-09-08 23:44:15 +0000

    update prod config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-  assetPrefix: isProd ? '/model-privacy-notice' : ''
+  assetPrefix: isProd ? 'https://1up.health/products/model-privacy-notice' : ''
 }
